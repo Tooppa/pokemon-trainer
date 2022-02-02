682ee81e4804901302069c5bce7d28ad3f97a5cf
Mikko <mikko.ryynanen2@gmail.com>
2022-02-02 19:38:21 +0200

    CHORE: Added environment variables via ngx-env/builder package

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://noroff-accelerate-api.herokuapp.com/trainers',
+  apiKey: process.env.NG_APP_API_KEY,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'https://noroff-accelerate-api.herokuapp.com/trainers',
+  apiKey: process.env.NG_APP_API_KEY,
 };
 
 /*
