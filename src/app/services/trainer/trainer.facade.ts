20cbcba42744a3d9c7435dc59c366239187b2f92
Mikko <mikko.ryynanen2@gmail.com>
2022-02-03 12:34:07 +0200

    FEAT: Session storage service implementation to trainer facade

--- src/app/services/trainer/trainer.facade.ts
+++ src/app/services/trainer/trainer.facade.ts
@@ -2,26 +2,35 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Trainer } from 'src/app/models/trainer.model';
 import { TrainerState } from 'src/app/state/trainer.state';
+import { SessionStorageService } from '../session/session-storage.service';
 import { TrainerAPIService } from './trainer-api.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TrainerFacade {
+  
   constructor(
     private readonly trainerApi: TrainerAPIService,
-    private readonly trainerState: TrainerState
+    private readonly trainerState: TrainerState,
+    private readonly sessionStorage: SessionStorageService
   ) {}
 
   /**
    * Must be called first before any other function is called
+   * NOTE: Overrides current sessionStorage
    */
+  // TODO: I think we can move this to when request things for the first time?
   public fetchTrainers(): void {
-    // TODO: I think we can move this to when request things for the first time?
-    // TODO Either fetch them from api or sessionStorage
-    this.trainerApi.getAllTrainers().subscribe((trainers: Trainer[]) => {
-      this.trainerState.setTrainers$(trainers);
-    });
+    if (this.sessionStorage.hasSessionSet) {
+      const storedTrainers = this.sessionStorage.loadSession<Trainer>();
+      this.trainerState.setTrainers$(storedTrainers);
+    } else {
+      this.trainerApi.getAllTrainers().subscribe((trainers: Trainer[]) => {
+        this.trainerState.setTrainers$(trainers);
+        this.sessionStorage.saveSession(trainers);
+      });
+    }
   }
 
   public postTrainer$(username: string): void {
