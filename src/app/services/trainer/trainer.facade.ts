858c5f6f70cf706bbe962f3deb65648ef4ad4004
Mikko <mikko.ryynanen2@gmail.com>
2022-02-03 14:44:39 +0200

    FEAT: added local storage service and implemented it in trainer facade

--- src/app/services/trainer/trainer.facade.ts
+++ src/app/services/trainer/trainer.facade.ts
@@ -1,41 +1,41 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Trainer } from 'src/app/models/trainer.model';
 import { TrainerState } from 'src/app/state/trainer.state';
 import { SessionStorageService } from '../session/session-storage.service';
+import { LocalStorageService } from '../storage/local-storage.service';
 import { TrainerAPIService } from './trainer-api.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TrainerFacade {
-  
+export class TrainerFacade implements OnInit {
   constructor(
     private readonly trainerApi: TrainerAPIService,
     private readonly trainerState: TrainerState,
-    private readonly sessionStorage: SessionStorageService
+    private readonly sessionStorage: SessionStorageService,
+    private readonly localStorage: LocalStorageService
   ) {}
+
+  ngOnInit(): void {
+    this.fetchTrainers();
+  }
 
   /**
    * Must be called first before any other function is called
    * NOTE: Overrides current sessionStorage
    */
-  // TODO: I think we can move this to when request things for the first time?
-  public fetchTrainers(): void {
-    if (this.sessionStorage.hasSessionSet) {
-      const storedTrainers = this.sessionStorage.loadSession<Trainer>();
-      this.trainerState.setTrainers$(storedTrainers);
-    } else {
-      this.trainerApi.getAllTrainers().subscribe((trainers: Trainer[]) => {
-        this.trainerState.setTrainers$(trainers);
-        this.sessionStorage.saveSession(trainers);
-      });
-    }
+  private fetchTrainers(): void {
+    this.trainerApi.getAllTrainers().subscribe((trainers: Trainer[]) => {
+      this.trainerState.setTrainers(trainers);
+      this.sessionStorage.saveSession(trainers);
+    });
   }
 
   public postTrainer$(username: string): void {
     this.trainerApi.postTrainer(username).subscribe((trainer: Trainer) => {
-      this.trainerState.setTrainer$(trainer);
+      this.trainerState.setTrainer(trainer);
+      this.localStorage.save(trainer);
     });
   }
 
@@ -43,8 +43,12 @@ export class TrainerFacade {
     this.trainerApi
       .addNewPokemon(username, newPokemon)
       .subscribe((trainer: Trainer) => {
-        this.trainerState.setTrainer$(trainer);
+        this.trainerState.setTrainer(trainer);
       });
+  }
+
+  get localTrainer(): Trainer | null {
+    return this.localStorage.load<Trainer>();
   }
 
   public trainers$(): Observable<Trainer[]> {
@@ -52,6 +56,6 @@ export class TrainerFacade {
   }
 
   public trainer$(username: string): Trainer | undefined {
-    return this.trainerState.getTrainer$(username);
+    return this.trainerState.getTrainer(username);
   }
 }
