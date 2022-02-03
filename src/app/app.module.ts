5f33bd7ccf8a399bc22c7cb12ee5923f13fc273f
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-02-03 14:52:52 +0200

    FEAT: routes and pages added. made a layout with header and footer that shows in every page and scales with the size of the content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { TrainerPageComponent } from './trainer-page/trainer-page.component';
+import { CataloguePageComponent } from './catalogue-page/catalogue-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    TrainerPageComponent,
+    CataloguePageComponent
   ],
   imports: [
     BrowserModule,
