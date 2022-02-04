eecff34cc9d338631629a2af7e124b88ae294785
Mikko <mikko.ryynanen2@gmail.com>
2022-02-04 13:06:25 +0200

    CHORE: Added pokemonImgBaseUrl to environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   trainersBaseUrl: 'https://noroff-accelerate-api.herokuapp.com/trainers',
   pokemonBaseUrl: 'https://pokeapi.co/api/v2/',
+  pokemonImgBaseUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/',
   apiKey: process.env.NG_APP_API_KEY,
 };
 
