b5ac849c8d846974e2d6b36dedc7bec8b156dc9a
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-02-04 16:45:08 +0200

    FEAT: fetching the pokemons now works

--- src/app/catalogue-page/catalogue-page.component.ts
+++ src/app/catalogue-page/catalogue-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Pokemon } from '../models/pokemon.model';
+import { PokemonFacade } from '../services/pokemon/pokemon.facade';
 
 @Component({
   selector: 'app-catalogue-page',
@@ -7,10 +9,17 @@ import { Pokemon } from '../models/pokemon.model';
   styleUrls: ['./catalogue-page.component.scss']
 })
 export class CataloguePageComponent implements OnInit {
-  pokemons: Array<Pokemon> = [];
-  constructor() { }
+  constructor(private readonly pokemonService: PokemonFacade) { }
+
+  get pokemon$(): Observable<Pokemon[]> {
+    console.log(this.pokemonService.pokemon$());
+    
+    return this.pokemonService.pokemon$();
+  }
+
 
   ngOnInit(): void {
+    this.pokemonService.getPokemon(0,200);
   }
 
 }
