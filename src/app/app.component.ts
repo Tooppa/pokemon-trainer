fb659a34745c415d829845cec3f0709151645c0f
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-02-07 08:34:32 +0200

    FEAT: notification when adding to your collection and conditional rendering to the header links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Trainer } from './models/trainer.model';
 import { TrainerFacade } from './services/trainer/trainer.facade';
 
@@ -8,9 +9,11 @@ import { TrainerFacade } from './services/trainer/trainer.facade';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'pokemon-trainer';
-
   constructor(private readonly trainerFacade: TrainerFacade) { }
+  
+  get currentTrainer$(): Observable<Trainer> {
+    return this.trainerFacade.currentTrainer;
+  }
 
   ngOnInit(): void {
     this.trainerFacade.loadStoredTrainer();
